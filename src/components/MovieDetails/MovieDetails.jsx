feb3a1de50a12b2bfaa38f6995170d0e76803f2e
Jonathan Palalay <ring5pun@hotmail.com>
2022-06-26 14:22:59 -0500

    fixed the append on MovieDetails

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -9,7 +9,13 @@ function MovieDetails(){
         <>
         <h1> Details </h1>
         <h3>
-            {movieDetails[0].title}
+        {movieDetails[0] && movieDetails[0].title}
+        <p>
+        <img src = {movieDetails[0] && movieDetails[0].poster} />
+        </p>
+        <p>
+        {movieDetails[0] && movieDetails[0].description}
+        </p>
         </h3>
 
         </>
